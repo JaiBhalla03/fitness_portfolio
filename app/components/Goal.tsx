a6944ad7ee42a02f7156239817e21d62c034761f
Jai Bhalla <jai.bhalla2021@vitstudent.ac.in>
2025-02-02 17:27:10 +0530

    adding the goald section

--- app/components/Goal.tsx
+++ app/components/Goal.tsx
@@ -1,7 +1,53 @@
 import React from 'react'
+import GoalCard from './utils/GoalCard'
+import Image from 'next/image';
+import goalImage from '../../public/images/goal-image.png';
+import { FaStar } from 'react-icons/fa';
 
 export default function Goal() {
   return (
-    <div>Goal</div>
+    <div className='text-white flex w-full p-12'>
+        <div className='relative bg-[#181920] rounded-3xl p-8 w-1/2'>
+            <Image src={goalImage} alt='' className='filter saturate-50 absolute top-0 right-0 h-full object-cover'/>
+            <div className='bg-[#80D20E] rounded-full p-4 text-black w-min'>
+                <FaStar size={30}/>
+            </div>
+            <div className='font-notable text-xl mt-8'>Success Stories</div>
+            <div className='w-1/2 my-20'>
+                I reached my fitness goals faster than ever with tailored workouts. Wrist notification keep me on track, even during session. Absolutely love this feature and how it enhances my experience
+            </div>
+            <div className='font-notable text-xl mt-8'>
+                Kende Attila
+            </div>
+            <div>CEO of Starbucks</div>
+            <div className='flex items-center text-[#80D20E] mt-10'>
+            <FaStar size={20}/>
+            <FaStar size={20}/>
+            <FaStar size={20}/>
+            <FaStar size={20}/>
+            <FaStar size={20} className='text-white'/>
+            </div>
+        </div>
+        <div className='w-1/2 pl-4'>
+            <div className='font-notable text-[3.5rem] leading-[3.5rem]'>Best Training Plan For You</div>
+            <div className='flex flex-col gap-8 mt-8'>
+                <GoalCard
+                    price='$19.99/Month'
+                    heading='Premium Plan'
+                    paragraph='The premium package desgined for dedicated fitness enthusiasts who seek the highest level of training'
+                />
+                <GoalCard
+                    price='$11.99/Month'
+                    heading='Elite Plan'
+                    paragraph='The Elite Plan for passionate fitness lovers, offering top-tier resources and exclusive benefits'
+                />
+                <GoalCard
+                    price='Free'
+                    heading='Basic Plan'
+                    paragraph='Our Basic Plan offers essential gym access, perfect for beginners or those with a simple workout routine'
+                />
+            </div>
+        </div>
+    </div>
   )
 }
---
+++ app/components/utils/GoalCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+interface GoalCardProps{
+    price: string;
+    heading: string;
+    paragraph: string;
+}
+
+export default function GoalCard({price, heading, paragraph} : GoalCardProps) {
+  return (
+    <div className='group bg-[#181920] text-white flex p-8 rounded-3xl gap-8 cursor-pointer'>
+        <div className='group-hover:bg-[#80D20E] bg-transparent text-[#80D20E] group-hover:text-blue-950 w-72 text-md h-14 flex items-center justify-center rounded-full font-bold p-4 transition-all duration-300'>
+            {price}
+        </div>
+        <div className='flex flex-col text-right gap-4'>
+            <div className='text-xl font-notable'>{heading}</div>
+            <div className='text-sm'>{paragraph}</div>
+        </div>
+    </div>
+  )
+}
